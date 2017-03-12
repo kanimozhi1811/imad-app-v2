ee287a504d1ca59e8ffa2009135357581267a97f
kanimozhi1811 <kani181196@gmail.com>
2017-03-12 20:13:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,12 +105,12 @@ app.get('/test-db',function(req,res)
 app.get('/articles/:articleName',function(req,res)
 {
   //var articleName=req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title="+req.param.articleName,function(err,result){
+    pool.query("SELECT * FROM article WHERE title= '" +req.param.articleName+"'",function(err,result){
     if(err){
         res.status(500).send(err.toString());
     }    else
     {
-        if(result.rows.length==0){
+        if(result.rows.length===0){
             res.status(404).send('Article not found');
         } else{
             var articleData=result.rows[0];
