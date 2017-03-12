3374a3ba2993c5ee8f9a81a9e739f15b5d94572b
kanimozhi1811 <kani181196@gmail.com>
2017-03-12 19:30:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={
+    user:'kanimozhi1811',
+    database:'kanimozhi1811',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -79,11 +87,21 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 }
 
-
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+app.get('/test-db',function(req,res)
+{
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+        });
+    });
 });
 app.get('/:articleName',function(req,res)
 {
